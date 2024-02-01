7382cf92f214a84b07a7f090a073e4a974dd4134
Syed_Hamid <syedhamid1491@gmail.com>
2024-02-01 08:37:33 +0530

    lab

--- BFS.py
+++ BFS.py
@@ -1,29 +1,49 @@
 from collections import defaultdict
+
 class Graph:
+ 
  def __init__(self):
+   
    self.graph = defaultdict(list)
+
  def addEdge(self,u,v):
+   
    self.graph[u].append((v))
    self.graph[v].append((u))
+
  def dispg(self):
+   
    print(self.graph.items())
+
  def BFS(self, s,goal):
+    
     visited = [False] * (len(self.graph))
     frontier_q = []
     frontier_q.append(s)
     visited[s] = True
+
     while frontier_q:
+
       s = frontier_q.pop(0)
       print (s, end = " ")
+
       if(s == goal):
+
         print("\n Goal found")
+
         return
+      
       for i in self.graph[s]:
+
         if visited[i] == False:
+
           frontier_q.append(i)
           visited[i] = True
           print("\n Goal not found")
+
+
 g = Graph()
+
 g.addEdge(0,1)
 g.addEdge(0,3)
 g.addEdge(1,2)
--- gbfs.py
+++ gbfs.py
@@ -1,25 +1,39 @@
 from queue import PriorityQueue
+
 v=14
 graph=[[]for i in range(v)]
+
 def best_first_search(actual_Src, target, n):
+
     visited=[False]*n
     pq=PriorityQueue()
     pq.put((0,actual_Src))
     visited[actual_Src]=True
+
     while pq.empty()==False:
+
         u=pq.get()[1]
         print(u,end="" )
+
         if u==target:
+
             break
     
         for v,c in graph[u]:
+
             if visited[v]==False:
+
                 visited[v]=True
                 pq.put((c,v))
+
         print()
+
 def addedge(x,y,heuristic):
+    
     graph[x].append((y,heuristic))
     graph[y].append((x,heuristic))
+
+
 addedge(0,1,4)
 addedge(0,2,2)
 addedge(1,3,11)
